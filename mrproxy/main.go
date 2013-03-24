6be7b10746da1a0d084cdda79c0f0bcbc9888b34
Damjan Cvetko <damjan.cvetko@gmail.com>
2013-03-24 14:56:31 +0100

    Removing old code.

--- mrproxy/main.go
+++ mrproxy/main.go
@@ -88,67 +88,6 @@ func processMc(c net.Conn, pool *redis.Pool) {
 				bw.WriteString(res.Protocol())
 				bw.Flush()
 			}
-			/*
-					case "get":
-						for i:= range req.Keys {
-							r, err := redis.Values(conn.Do("MGET", req.Keys[i], req.Keys[i]+"_mcflags"))
-							if err != nil {
-								// check for error type.
-								e := protocol.GetCache(req.Keys[i])
-			log.Println(e)
-								if e != nil {
-									bw.WriteString(fmt.Sprintf("VALUE %s %s %d\r\n", e.Key, e.Flags, len(e.Data)))
-									bw.Write(e.Data)
-									bw.WriteString("\r\n")
-									continue
-								}
-								bw.WriteString("SERVER_ERROR "+err.Error()+"\r\n")
-								bw.Flush()
-								log.Println(err)
-								continue;
-							}
-							if r[0] != nil {
-								data, err := redis.Bytes(r[0], nil)
-								flags, err := redis.String(r[1], err)
-								if err != nil {
-									bw.WriteString("SERVER_ERROR "+err.Error()+"\r\n")
-									bw.Flush()
-									log.Println(err)
-									continue;
-								}
-								// todo, both can return error
-								bw.WriteString(fmt.Sprintf("VALUE %s %s %d\r\n", req.Keys[i], flags, len(data)))
-								bw.Write(data)
-								bw.WriteString("\r\n")
-							}
-						}
-						bw.WriteString("END\r\n");
-						bw.Flush()
-					case "set":
-						protocol.AddCache(protocol.NewMcEntry(req.Key, req.Flags, req.Exptime, req.Data))
-
-						r, err := redis.String(conn.Do("MSET", req.Key, req.Data, req.Key+"_mcflags", req.Flags))
-						if err != nil || r != "OK" {
-							bw.WriteString("SERVER_ERROR "+r+"\r\n")
-							bw.Flush()
-							log.Println(err)
-							continue;
-						}
-
-						if req.Exptime != 0 {
-							_, err = conn.Do("EXPIREAT", req.Key, req.Exptime)
-							if err != nil {
-								bw.WriteString("SERVER_ERROR storing expire time\r\n")
-								bw.Flush()
-								log.Println(err)
-								continue;
-							}
-						}
-
-						// err
-						bw.WriteString("STORED\r\n")
-						bw.Flush()
-			*/
 		}
 	}
 }
